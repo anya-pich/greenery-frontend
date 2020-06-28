09be15e4d659a030150422c5c3a8cd4e2eddb38b
anya-pich <anna.pich@outlook.com>
2020-06-27 18:24:57 -0700

    set up apollo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
-}
-
-export default HomePage
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import withApollo from '../lib/withApollo';
+ 
+const QUERY = gql`
+  {
+    title
+  }
+`;
+ 
+const Index = () => {
+  const { loading, data } = useQuery(QUERY);
+ 
+  if (loading || !data) {
+    return <h1>loading...</h1>;
+  }
+  return <h1>{data.title}</h1>;
+};
+ 
+export default withApollo(Index);
